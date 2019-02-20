14815bd0f45a47dfb8e18a9677f52b25ff97daf7
DaCh0rt <ianharvey95@gmail.com>
2019-02-20 18:42:05 -0500

    Update game.java

--- game/game.java
+++ game/game.java
@@ -4,14 +4,17 @@ public class game{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 
+		//read in num cases
 		int n = in.nextInt();
 		in.nextLine();
 
+		//solve all cases
 		for(int i = 0; i < n; i++){
 			int max = 0;
 			int cur = 0;
 			String input = in.nextLine();
 
+			//run thru the input string and find the max height with the given input
 			for(int j = 0; j < input.length(); j++){
 				if(input.charAt(j) == 'v')
 					cur++;
@@ -21,7 +24,8 @@ public class game{
 				if(cur > max)
 					max = cur;
 			}
-
+	
+			//done, print out answer
 			System.out.println(max);
 		}
 	}
