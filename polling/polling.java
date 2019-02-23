fb349cdc498143e0aff4ba5020ecefa38ffbbd41
DaCh0rt <ianharvey95@gmail.com>
2019-02-22 20:18:06 -0500

    Update polling.java

--- polling/polling.java
+++ polling/polling.java
@@ -2,12 +2,16 @@ import java.util.*;
 
 public class polling{
 	public static void main(String[] args){
+		
 		Scanner in = new Scanner(System.in);
+		//read in num of people
 		int n = in.nextInt();
 		int max = 0;
 
+		//keep track of everyone
 		HashMap<String, Integer> election = new HashMap<String, Integer>();
 
+		//read in the votes
 		for(int i = 0; i < n; i++){
 			String person = in.next();
 
@@ -18,13 +22,15 @@ public class polling{
 			}
 		}
 
+		//get everyone from the map
 		ArrayList<person> result = new ArrayList<person>();
 		for(String name: election.keySet()){
 			result.add(new person(name, election.get(name)));
 		}
-
+		
+		//sort and find who won
 		Collections.sort(result);
-		int winning = result.get(0).votes;
+		int winning = result.get(0).votes;//find winning number, could be a tie so check if more than 1
 		for (int i=0; i<result.size(); i++){
 			if(result.get(i).votes < winning)
 				break;
@@ -48,10 +54,10 @@ class person implements Comparable<person>{
 	}
 
 	public int compareTo(person other){
-		if(this.votes != other.votes){
+		if(this.votes != other.votes){//check votes
 			return other.votes - this.votes;
 		} else {
-			return this.name.compareTo(other.name);
+			return this.name.compareTo(other.name);//if a tie then sort by name
 		}
 	}
 }
