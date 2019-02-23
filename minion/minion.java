bf0294d2cc7e8575c65a6740e75f995a567fb92f
DaCh0rt <ianharvey95@gmail.com>
2019-02-22 19:52:50 -0500

    Update minion.java

--- minion/minion.java
+++ minion/minion.java
@@ -10,28 +10,33 @@ public class minion{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 
+		//read in num cases and run thru them
 		int m = in.nextInt();
-
 		for(int i = 0; i < m; i++){
+			
+			//read in all the trials the ninja can't do
 			int t = in.nextInt();
 			bad = new String[t];
 			for(int j = 0; j < t; j++){
 				bad[j] = in.next();
 			}
 
+			//read in edges and nodes
 			n = in.nextInt();
 			int e = in.nextInt();
 
+			//adjmat and parallel array to store trials on edges
 			adjMat = new boolean[n][n];
 			adjMatT = new String[n][n];
 
+			//init
 			for(int j = 0; j < n; j++){
 				for(int k = 0; k < n; k++){
 					adjMatT[j][k] = "";
 				}
 			}
 
-
+			//read
 			for(int j = 0; j < e; j++){
 				int x = in.nextInt();
 				int y = in.nextInt();
@@ -43,29 +48,16 @@ public class minion{
 				adjMatT[y][x] = s;
 			}
 
+			//assume not possible and dfs
 			solved = false;
 			DFS();
 		}
 	}
 
-	static void DFS() {
+	static void DFS() {//wrapper
 		boolean[] v = new boolean[n];
-		int numComponents = 0;
 
-		// for(int i = 0; i < n; i++){
-		// 	if(!v[i]){
-		// 		++numComponents;
-		// 		// System.out.printf("DFS for component %d starting at node %d%n", numComponents,i);
-		// 		DFS(i,v);
-		// 	}
-		// }
 		DFS(0,v);
-
-		// boolean solved = true;
-		// for(int i = 0; i < v.length; i++){
-		// 	if(v[i] == false)
-		// 		solved = false;
-		// }
 
 		if(solved){
 			System.out.println(1);
@@ -78,13 +70,17 @@ public class minion{
 		// System.out.printf("at node %d in the DFS%n",at);
 
 		v[at] = true;
-
+		
+		//we made it to the end
 		if(at == (n - 1)){
 			solved = true;
 			return;
 		}
 
+		//go thru nodes
 		for(int i = 0; i<n; i++){
+			
+			//see if there is an unsurmountable challenge
 			boolean good = true;
 			for(int j = 0; j < bad.length; j++){
 				if(bad[j].compareTo(adjMatT[at][i]) == 0){
@@ -92,7 +88,8 @@ public class minion{
 					break;
 				}
 			}
-			// System.out.println("maybe");
+			
+			//looks ok
 			if(adjMat[at][i] && !v[i] && good){
 				// System.out.printf("Going to node %d...", i);
 				DFS(i,v);
