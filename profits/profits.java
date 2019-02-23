0b500a331399f98db499bff99ada59a484491011
DaCh0rt <ianharvey95@gmail.com>
2019-02-22 20:22:07 -0500

    Update profits.java

--- profits/profits.java
+++ profits/profits.java
@@ -4,12 +4,17 @@ public class profits{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 
+		//guranteed 1 test case, find num of days, test cases end with 0
 		int n = in.nextInt();
 		while(n > 0){
+			
+			//read in all days
 			int[] days = new int[n];
 			for(int i = 0; i < n; i++){
 				days[i] = in.nextInt();
 			}
+			
+			//calculate maximum subsequence sum
 			System.out.println(mcss3(days));
 
 			n = in.nextInt();
@@ -17,64 +22,9 @@ public class profits{
 
 	}
 
-
-
-	public static int mcss(int[] vals) {
-		
-		int max=(int) -1e9, start=0, end=0, sum=0;
-		
-		// All starting values.
-		for (int i=0; i<vals.length; i++) {
-			
-			// All ending values.
-			for (int j=i; j<vals.length; j++) {
-			
-				sum =0;
-				// Add up everything in between.
-				for (int k=i; k<=j; k++)
-					sum += vals[k];
-			
-				// Update if we find something better.
-				if (sum > max) {
-					max = sum;
-					start = i;
-					end = j;
-				
-				}
-			}
-		}
-		return max;
-	}
-
-	public static int mcss2(int[] vals) {
-		
-		int max=(int)-1e9, start=0, end=0, sum=0;
-		
-		// Loop through all start points.
-		for (int i=0; i<vals.length; i++) {
-			sum = 0;
-			
-			// And all end points.
-			for (int j=i; j<vals.length; j++) {
-				
-				// Just add the current value to the running sum to obtain
-				// this new sum.
-				sum += vals[j];
-			
-				// Update if we get a better max.
-				if (sum > max) {
-					max = sum;
-					start = i;
-					end = j;
-				
-				}
-			}
-		}
-		return max;
-	}
-
 	public static int mcss3(int[] vals) {
 	
+		//need to init max to the first value to deal with the case of all negatives
 		int max=vals[0], sum=0, start=0, end=0, i=0;
 	
 		for (int j=0; j<vals.length; j++) {
