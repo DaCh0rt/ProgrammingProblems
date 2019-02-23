2ff042cea0d7283a1baa82de3a2d6cf49130d377
DaCh0rt <ianharvey95@gmail.com>
2019-02-22 19:56:53 -0500

    Update monkey.java

--- monkey/monkey.java
+++ monkey/monkey.java
@@ -4,13 +4,18 @@ public class monkey{
 
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
+		
+		//get num cases and get rid of '\n' 
 		int n = in.nextInt();
 		in.nextLine();
 
+		//run thru cases
 		for(int i = 0; i < n; i++){
+			//store string tree
 			String s = in.nextLine();
 			int tmp = 0;
 			int max = 0;
+			//loop thru tree and find max depth
 			for(int j = 0; j < s.length(); j++){
 				if(s.charAt(j) == '[')
 					tmp++;
@@ -21,6 +26,7 @@ public class monkey{
 					max = tmp;
 			}
 
+			//finished you need 2 ^ max depth monkeys
 			System.out.println((i + 1) + " " + (int)Math.pow(2,max));
 		}
 	}
