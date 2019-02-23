d21c2c0882a19e37956e7b2fa9e3bc85c41fbac9
DaCh0rt <ianharvey95@gmail.com>
2019-02-22 19:54:18 -0500

    Update minutes.java

--- minutes/minutes.java
+++ minutes/minutes.java
@@ -2,15 +2,18 @@ import java.util.*;
 
 public class minutes{
 	public static void main(String[] args){
+		//num cases, days, hours, minutes
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int d,h,m,total;
 
+		//go thru all cases
 		for(int i = 0; i < n; i++){
 			d = in.nextInt();
 			h = in.nextInt();
 			m = in.nextInt();
 
+			//quik mafs
 			total = m + h*60 + d*60*24;
 			System.out.println(total);
 		}
