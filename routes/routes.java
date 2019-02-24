3d3c373fdbaa36c6ba1e6d0b25142efc163537ae
Ian Harvey <ianharvey95@gmail.com>
2019-02-23 21:17:28 -0500

    Update routes.java

--- routes/routes.java
+++ routes/routes.java
@@ -6,14 +6,18 @@ public class routes{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 
+		//read in num cases and run thru them
 		int c = in.nextInt();
 		for(int i = 0; i < c; i++){
+			//read in vertexes, edges, and number of shortest paths to source to check.
 			int v = in.nextInt();
 			int e = in.nextInt();
 			int q = in.nextInt();
 
+			//all edges are 2 way
 			edge[] eList = new edge[e*2];
 
+			//read in all edges
 			for(int j = 0; j < e; j++){
 				int s = in.nextInt() - 1;
 				int d = in.nextInt() - 1;
@@ -23,11 +27,14 @@ public class routes{
 				eList[2*j + 1] = new edge(d,s,w);
 			}
 
+			//run bellman ford
 			int[] sol = bellmanford(eList, v, 0);
 
+			//distance from source to dest is the same as dest to source in a 2 way graph
 			for(int j = 0; j < q; j++){
 				int dest = in.nextInt() -1;
 
+				//print out distance from dest to source
 				System.out.println(sol[dest]);
 			}
 
